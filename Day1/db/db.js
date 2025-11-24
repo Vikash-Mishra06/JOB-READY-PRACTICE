f3a93db07ca47bfb1a1e299259863b4c581246f7
Vikash Mishra <mishravikash0612@gmail.com>
2025-11-24 18:46:06 +0530

    Practiced CRUD API

---
+++ Day1/db/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('Connected to DB');
+    } catch (error) {
+        console.log('Error while connecting to DB', error);
+    }
+}
+
+export default connectDB/
